bb8655a9e0a182e61932ad6bc1d2e3dfa215ec22
Pedro Henrique <ph8584774@gmail.com>
2024-06-16 18:32:48 -0300

    Project created and gitignore updated

---
+++ SBPJ/src/main/java/com/example/SBJ/Project/Greeting.java
@@ -0,0 +1,16 @@
+package com.example.SBJ.Project;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+
+}
---
+++ SBPJ/src/main/java/com/example/SBJ/Project/SbjProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SBJ.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbjProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbjProjectApplication.class, args);
+	}
+
+}
