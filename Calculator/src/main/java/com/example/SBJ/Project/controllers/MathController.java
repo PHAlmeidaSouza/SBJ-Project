d05ca428b39f28cd03a523a421ca97d7cdf5e2ea
Pedro Henrique <ph8584774@gmail.com>
2024-07-03 20:04:20 -0300

    Refactoring operations

--- Calculator/src/main/java/com/example/SBJ/Project/controllers/MathController.java
+++ Calculator/src/main/java/com/example/SBJ/Project/controllers/MathController.java
@@ -1,6 +1,8 @@
-package com.example.SBJ.Project;
+package com.example.SBJ.Project.controllers;
 
+import com.example.SBJ.Project.converters.NumberConverter;
 import com.example.SBJ.Project.exceptions.UnsupportedMathOperationException;
+import com.example.SBJ.Project.math.SimpleMath;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -8,8 +10,9 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class MathController {
 
-    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+
+    private SimpleMath math = new SimpleMath();
 
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(
@@ -17,9 +20,9 @@ public class MathController {
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
@@ -28,9 +31,9 @@ public class MathController {
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
-        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+        return math.subtraction(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
@@ -39,9 +42,9 @@ public class MathController {
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
-        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+        return math.multiplication(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
@@ -50,9 +53,9 @@ public class MathController {
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
-        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+        return math.division(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @RequestMapping(value = "/average/{numberOne}/{numberTwo}", method = RequestMethod.GET)
@@ -61,31 +64,19 @@ public class MathController {
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
-        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+        return math.average(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @RequestMapping(value = "/sqrt/{number}", method = RequestMethod.GET)
     public Double sqrt(
-            @PathVariable(value = "numberOne") String number
+            @PathVariable(value = "number") String number
     ) throws Exception {
 
-        if (!isNumeric(number))
+        if (!NumberConverter.isNumeric(number))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
-        return Math.sqrt(convertToDouble(number));
+        return math.sqrt(NumberConverter.convertToDouble(number));
     }
 
-    private Double convertToDouble(String strNumber) {
-        if (strNumber == null) return 0D;
-        String number = strNumber.replaceAll(",", ".");
-        if (isNumeric(number)) return Double.parseDouble(number);
-        return 0D;
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if (strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-    }
 }
---
+++ Calculator/src/main/java/com/example/SBJ/Project/math/SimpleMath.java
@@ -0,0 +1,29 @@
+package com.example.SBJ.Project.math;
+
+public class SimpleMath {
+
+    public Double sum(Double numberOne,Double numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    public Double subtraction(Double numberOne, Double numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    public Double multiplication(Double numberOne, Double numberTwo) {
+        return numberOne * numberTwo;
+    }
+
+    public Double division(Double numberOne, Double numberTwo) {
+        return numberOne / numberTwo;
+    }
+
+    public Double average(Double numberOne, Double numberTwo) {
+        return (numberOne + numberTwo) / 2;
+    }
+
+    public Double sqrt(Double number) {
+        return Math.sqrt(number);
+    }
+    
+}
