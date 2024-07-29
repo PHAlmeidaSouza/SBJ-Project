eb7b6ccdb3607b36849e387219ab8b6a368f7f60
Pedro Henrique <ph8584774@gmail.com>
2024-07-29 16:44:08 -0300

    :sparkles: feat: Implementing repository

---
+++ Calculator/src/main/java/com/SBJ/Project/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.SBJ.Project.repositories;
+
+import com.SBJ.Project.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
