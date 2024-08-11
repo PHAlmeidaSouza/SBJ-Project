ca27bf786a81ecb316f83401d578cc00c238dd73
Pedro Henrique de Almeida Souza <86302626+PHAlmeidaSouza@users.noreply.github.com>
2024-08-11 19:26:47 -0300

    Delete Calculator directory

--- Calculator/src/main/java/com/SBJ/Project/repositories/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SBJ.Project.repositories;
-
-import com.SBJ.Project.models.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
